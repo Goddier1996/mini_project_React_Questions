196b7ec3bff2a8cf1b490ec71f06476cb598b305
artium_96 <artium20@gmail.com>
2023-10-04 19:24:53 +0300

    add text when answer is incorrect

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,12 +1,11 @@
-import React from 'react'
 import { useQuiz } from "../context/Context";
 
 
 const Options = ({ question }) => {
 
-
     const { dispatch, answer } = useQuiz();
 
+    // check if have answer
     const hasAnswered = answer !== null;
 
 
@@ -14,17 +13,23 @@ const Options = ({ question }) => {
         <div className="options">
             {question.options.map((option, index) => (
                 <button
-                    className={`btn btn-option ${index === answer ? "answer" : ""} ${hasAnswered
-                        ? index === question.correctOption
-                            ? "correct"
-                            : "wrong"
-                        : ""
+                    className={`btn btn-option 
+                    ${index === answer && index !== question.correctOption ? "answer" : ""}
+                     ${hasAnswered
+                            ? index === question.correctOption
+                                ? "correct"
+                                : "wrong"
+                            : ""
                         }`}
                     key={option}
                     disabled={hasAnswered}
                     onClick={() => dispatch({ type: "newAnswer", payload: index })}
                 >
                     {option}
+
+                    {index === answer && index !== question.correctOption ?
+                        <p>Your answer is incorrect !</p>
+                        : ""}
                 </button>
             ))}
         </div>
